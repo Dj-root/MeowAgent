6ebaeb22eba0478c3dca9f19d5174aa2087497bf
holms <holms.html@gmail.com>
2018-02-15 01:30:59 +0200

    Created few methods

--- AgentPrototype/Program.cs
+++ AgentPrototype/Program.cs
@@ -8,8 +8,66 @@ namespace AgentPrototype
 {
     class Program
     {
+
         static void Main(string[] args)
         {
+            try
+            {
+                InitialConfig();
+            }
+            catch (NotImplementedException e)
+            {
+                Console.WriteLine("The called method was not implemented");
+                Console.WriteLine(e.Message);
+            }
+
+
+
+
+
+
+
+            Console.ReadLine();
+
+        }
+
+        static void InitialConfig()
+        {
+            string prgName = "Moo Agent";
+            bool autoConfig = false;
+            bool manualConfig = false;
+
+
+            Console.WriteLine("***** Welcome to {0} *****", prgName);
+
+            autoConfig = UsingXmlConfig();
+            if (autoConfig)
+            {
+                Console.WriteLine("Program started using config file.\nHere is initial config:");
+
+                return;
+            }
+
+            manualConfig = ManualConfig();
+            if (manualConfig)
+            {
+                Console.WriteLine("Program started using manual configuration.\nHere is initial config:");
+            }
+            else
+            {
+                Console.WriteLine("Something wrong with configuration");
+            }
+
+        }
+
+        static bool ManualConfig()
+        {
+            throw new NotImplementedException();
+        }
+
+        static bool UsingXmlConfig()
+        {
+            throw new NotImplementedException();
         }
     }
 }
